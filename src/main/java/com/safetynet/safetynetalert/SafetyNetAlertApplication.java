29984b84c947b410c8f077d94066373f58d94756
Myohres <77498044+Myohres@users.noreply.github.com>
2021-08-26 01:50:59 +0200

    add Generator List

--- src/main/java/com/safetynet/safetynetalert/SafetyNetAlertApplication.java
+++ src/main/java/com/safetynet/safetynetalert/SafetyNetAlertApplication.java
@@ -1,8 +1,16 @@
 package com.safetynet.safetynetalert;
 
+import com.jsoniter.any.Any;
+import com.safetynet.safetynetalert.constant.DataJson;
+import com.safetynet.safetynetalert.json.FireStationJson;
+import com.safetynet.safetynetalert.json.ListGenerator;
+import com.safetynet.safetynetalert.json.MedicalRecordJson;
+import com.safetynet.safetynetalert.json.PersonJson;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SafetyNetAlertApplication implements CommandLineRunner {
@@ -14,6 +22,15 @@ public class SafetyNetAlertApplication implements CommandLineRunner {
     @Override
     public void run(final String... args) {
         // Extraire les données JSON
+        Any jsonInput = DataJson.inputJson();
+        if (jsonInput != null) {
+            List<PersonJson> personJsonList =
+                    ListGenerator.personsList(jsonInput);
+            List<FireStationJson> fireStationJsons =
+                    ListGenerator.fireStationList(jsonInput);
+            List<MedicalRecordJson> medicalRecordJsons =
+                    ListGenerator.medicalRecordsList(jsonInput);
+        }
         // Modéliser ces données
         // Insérer les données dans la BDD
     }
--- src/main/java/com/safetynet/safetynetalert/package-info.java
+++ src/main/java/com/safetynet/safetynetalert/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * Extraction Json data, Generate entity, send entity to H2 Database.
+ */
 package com.safetynet.safetynetalert;
