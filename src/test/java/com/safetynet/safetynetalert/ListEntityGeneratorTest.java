f3134e68956b6f5f28b21045f1efb253c7268044
Myohres <77498044+Myohres@users.noreply.github.com>
2021-08-25 22:57:21 +0200

    Test Generator entity list beginning

---
+++ src/test/java/com/safetynet/safetynetalert/ListEntityGeneratorTest.java
@@ -0,0 +1,29 @@
+package com.safetynet.safetynetalert;
+
+import org.junit.jupiter.api.Test;
+
+
+public class ListEntityGeneratorTest {
+
+
+
+
+
+    @Test
+    public void PersonsEntityListTest() {
+        //List<PersonEntity> personEntityListTest = ListEntityGenerator.personsEntityList();
+    }
+
+
+    @Test
+    public void FireStationEntityListTest() {
+
+
+        //List<FireStationEntity> fireStationEntityList = ListEntityGenerator.fireStationEntityList();
+        /* qu'est ce qu'on attend d'une fireStation entity List
+        -> une entrée une station unique ac les adress associé
+         nombre d"entré égal le nombre de station*/
+        /* adress rangé au bon endroit*/
+    }
+
+}
