8d1fdc0fb8698c8b9c76b36e7cb1512f3880f356
Myohres <77498044+Myohres@users.noreply.github.com>
2021-08-25 22:44:07 +0200

    Setter update with long parse

--- src/main/java/entity/FireStationEntity.java
+++ src/main/java/entity/FireStationEntity.java
@@ -9,4 +9,9 @@ public class FireStationEntity {
 
     private Long station;
     private List<String> address;
+
+    public void setStation(final String stationInput) {
+        this.station = Long.parseLong(stationInput);
+    }
+
 }
